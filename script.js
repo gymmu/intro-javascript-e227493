141030becec1083d17376e31f267fb61e74a5f17
Dishan Selvanantham <e227493@edu.sbl.ch>
2023-10-17 11:24:31 +0200

    Aufgabe 3 gelöst

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ function changeBackground() {
   const body = document.querySelector("body")
   body.style.backgroundColor = "darkgrey"
 }
+
+function deleteText() { 
+  const elemtodeletetext = document.querySelector("#to-delete")
+elemtodeletetext.textContent = "Inhalt gelöscht"
+}
